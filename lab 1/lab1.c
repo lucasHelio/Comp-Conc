d71b0b49f69b8ee2aec224ba00f75ca19eed16d9
lucasHelio <lucasheliosr@hotmail.com>
2021-11-22 22:04:25 -0300

    lab 1

---
+++ lab 1/lab1.c
@@ -0,0 +1,77 @@
+
+//atividade 5
+/**
+ * Objetivo: Implementar um programa concorrente, com 
+ * duas threads (alem da thread principal), para elevar 
+ * ao quadrado cada elemento de um vetor de 10000 
+ * elementos. (Para cada elemento (ai) do vetor, 
+ * calcular (ai)^2 e escrever o resultado na mesma 
+ * posição do elemento.)
+ */
+
+
+/**
+ * bugs:
+ * descobrir pq ident n esta passando o valor esperado
+ * 
+ * Como dividir a tarefa em dois threads?
+ */
+
+#include<stdio.h>
+#include <pthread.h>
+
+#define NTHREADS 2
+#define TAMANHOVETOR 10000
+
+int vetor[TAMANHOVETOR];
+
+//função que eleva ao quadrado
+void * expoente2(void * arg){
+    int ident = *(int *)arg;
+
+    if(ident == 1)
+    {
+        for(int i =0; i<TAMANHOVETOR/2;i++)
+            vetor[i]=(i+1)*(i+1);
+    }
+    else
+    {
+        for(int i= TAMANHOVETOR/2;i<TAMANHOVETOR;i++)
+            vetor[i]=(i+1)*(i+1);
+    }
+    
+    pthread_exit(NULL);
+
+}
+
+int main()
+{
+    pthread_t tid[NTHREADS];
+    int ident [NTHREADS]; //identificador local da thread
+    
+    
+
+    //temos dois threads que fariam a função de elevar // pensar em como resolver
+    //elementos do vetor ao quadrado
+    for(int i =0;i<NTHREADS;i++)
+    {
+        ident[i]= i+1;
+        pthread_create(&tid[i], NULL, expoente2, (void *)&ident[i]);  
+    }
+
+    for(int i =0;i<NTHREADS;i++)
+    {
+        if(pthread_join(tid[i], NULL))
+            printf("Erro: pthread create - tid[%d] = %lu", i, tid[i]);
+
+    }
+
+
+    //teste
+    for(int i=0; i<TAMANHOVETOR;i++){
+        printf("vetor[%d] = %d\n", i, vetor[i]);
+    }
+
+    pthread_exit(NULL);
+    return 0;
+}
